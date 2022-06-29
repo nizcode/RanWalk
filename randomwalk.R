5b6cd17deae6651ed246b955e3553ad5fc4da3fc
nizcode <neilfa99@gmail.com>
2022-06-29 23:11:26 +0100

    benchmarked my 2 random walk funciton

--- randomwalk.R
+++ randomwalk.R
@@ -36,15 +36,21 @@ sum(yes20)/1000 #probability is 0.11 of reaching destination within walking 20 r
 #Question 3
 #compare the two walk functions
 library(rbenchmark)
-benchmark(walk(100,2),walk2(100,2),replications(100),order = "relative") #benchmark error this wont work
-
+benchmark(walk(100,2),walk2(100,2),replications(100),order = "relative") 
+#error using benchmark error so it wont work - "Error in formula.default(object, env = baseenv()) : invalid formula"
+#-------
+#Using microbenchmark instead
+library(microbenchmark)
+microbenchmark(walk1 = {walk(100000,2)},walk2 = {walk2(100000,2)}, times = 1000L, unit = "milliseconds")
+#interesting to see my second walk function is more efficient
+#------
 
 
 #Question 4
 #Manhattan distance
 avgD <- c()
-for(i in 1:100000){
-  avgD<-c(avgD,ManhattanD(50,23))
+for(i in 1:1000){
+  avgD<-c(avgD,ManhattanD(50))
 }
 mean(avgD) # = 11 for walk2 and 15 for walk
 #my worry here is tha the walk functions are equivalent
